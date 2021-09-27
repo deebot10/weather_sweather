43832957edad15e87c3251821fe68bbca72ef717
deebot10 <dee1jhill@gmail.com>
2021-09-27 09:20:02 -0600

    Create request test/route for library

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
       resources :backgrounds, only: [:index] 
       resources :users, only: [:create]
       resources :sessions
+      resources 'book_search'
+      # get '/booksearch', to: 'book-search#index' 
     end
   end
 end
---
+++ spec/requests/api/v1/library_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'Library Page', :vcr do
+  describe 'Displays the book info' do
+    # before(:each) do
+    # end
+    
+    it 'Can request the Books by location' do
+      params = {location: "denver,co", quantity: 5}
+      headers = {"CONTENT_TYPE": "application/json", "ACCEPT": "application/json"}
+      get "/api/v1/book_search", headers: headers, params: params  
+      forecast = JSON.parse(response.body, symbolize_name: true)
+    end
+  end
+end
