1c394c3ac6b0fd96da30217768505d9a62ea2af9
deebot10 <dee1jhill@gmail.com>
2021-09-27 14:52:52 -0600

    Draft User login request test

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::SessionsController < ApplicationController
+  def create
+    user = User.find_by(email: params[:email]) 
+
+    
+  end
+end
--- spec/requests/api/v1/login_spec.rb
+++ spec/requests/api/v1/login_spec.rb
@@ -4,9 +4,16 @@ RSpec.describe 'Login Page', :vcr do
   describe 'Create an account for users' do
     before(:each) do
       @headers = {"CONTENT_TYPE": "application/json", "ACCEPT": "application/json"}
+      body = {
+              "email": "whatever@example.com",
+              "password": "password",
+              "password_confirmation": "password"
+      }
+      post "/api/v1/users", headers: @headers, params: JSON.generate(body)
+      user = JSON.parse(response.body, symbolize_name: true)
     end
     
-    xit 'Can request the User creation' do
+    it 'Can request the User creation' do
       body = {
               "email": "whatever@example.com",
               "password": "password",
