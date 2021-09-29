56cb30b5b378f79d39a48ce5feb137a8d8c5203e
deebot10 <dee1jhill@gmail.com>
2021-09-28 19:53:17 -0600

    Add sadPath testing to background/request_spec

--- spec/requests/api/v1/backgrounds_spec.rb
+++ spec/requests/api/v1/backgrounds_spec.rb
@@ -2,14 +2,22 @@ require 'rails_helper'
 
 RSpec.describe 'Background Page', :vcr do
   describe 'Displays the background info' do
-    # before(:each) do
-    # end
     
     it 'Can request the Forecast by location' do
       params = {query: "denver,co"}
       headers = {"CONTENT_TYPE": "application/json", "ACCEPT": "application/json"}
       get "/api/v1/backgrounds", headers: headers, params: params  
       background = JSON.parse(response.body, symbolize_name: true)
+    end
+
+     it 'sad path: Cannot return a background' do
+      # params = {query: "denver,co"}
+      headers = {"CONTENT_TYPE": "application/json", "ACCEPT": "application/json"}
+      get "/api/v1/backgrounds", headers: headers  
+      background = JSON.parse(response.body, symbolize_name: true)
+     
+      expect(background['message']).to eq('unsuccessful')
+      expect(background['error']).to eq('Unable to find location')
     end
   end
 end
